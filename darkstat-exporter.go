7152dacf5332a0dffa47129825d1d07e85e13636
Mariusz Jędrzejewski <mariusz.jedrzejewski@gmail.com>
2023-03-26 17:44:29 +0200

    dodatnie "group" do metryki

--- darkstat-exporter.go
+++ darkstat-exporter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,30 +25,46 @@ type HostData struct {
 	Total      float64
 }
 
+type ConfigEntry struct {
+	Group string   `json:"group"`
+	Ip    []string `json:"ip"`
+}
+
+type Config struct {
+	Cfg []ConfigEntry `json:"cfg"`
+}
+
+var config Config
+
 var (
 	inBytesCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "darkstat_in_bytes",
 			Help: "Incoming bytes",
 		},
-		[]string{"ip", "hostname", "mac_address"})
+		[]string{"group", "ip", "hostname", "mac_address"})
 
 	outBytesCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "darkstat_out_bytes",
 			Help: "Outgoing bytes",
 		},
-		[]string{"ip", "hostname", "mac_address"})
+		[]string{"group", "ip", "hostname", "mac_address"})
 
 	totalBytesCounter = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "darkstat_total_bytes",
 			Help: "Total bytes",
 		},
-		[]string{"ip", "hostname", "mac_address"})
+		[]string{"group", "ip", "hostname", "mac_address"})
 )
 
 func init() {
+	err := json.Unmarshal([]byte(os.Getenv("CONFIG")), &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Registering prometheus metrics...")
 	prometheus.MustRegister(inBytesCounter)
 	prometheus.MustRegister(outBytesCounter)
@@ -69,31 +86,35 @@ func init() {
 	s.StartAsync()
 }
 
+func parseConfig() {
+
+}
+
 var recordMetrics = func() {
-	ipList := strings.Fields(os.Getenv("IP_LIST"))
+	for _, cfg := range config.Cfg {
+		for _, ip := range cfg.Ip {
+			url := fmt.Sprintf(os.Getenv("DARKSTAT_URL_PREFIX"), ip)
 
-	for _, ip := range ipList {
-		url := fmt.Sprintf(os.Getenv("DARKSTAT_URL_PREFIX"), ip)
-
-		doc, err := goquery.NewDocument(url)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		dataToParse := make([]string, 0)
-
-		doc.Find("p").Each(func(x int, p *goquery.Selection) {
-			if x == 0 || x == 2 {
-				dataToParse = append(dataToParse, strings.Split(p.Text(), "\n")...)
+			doc, err := goquery.NewDocument(url)
+			if err != nil {
+				log.Println(err)
+				return
 			}
-		})
 
-		d := getValues(dataToParse)
+			dataToParse := make([]string, 0)
 
-		inBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.In))
-		outBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.Out))
-		totalBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.Total))
+			doc.Find("p").Each(func(x int, p *goquery.Selection) {
+				if x == 0 || x == 2 {
+					dataToParse = append(dataToParse, strings.Split(p.Text(), "\n")...)
+				}
+			})
+
+			d := getValues(dataToParse)
+
+			inBytesCounter.WithLabelValues(cfg.Group, ip, d.Hostname, d.MacAddress).Set(float64(d.In))
+			outBytesCounter.WithLabelValues(cfg.Group, ip, d.Hostname, d.MacAddress).Set(float64(d.Out))
+			totalBytesCounter.WithLabelValues(cfg.Group, ip, d.Hostname, d.MacAddress).Set(float64(d.Total))
+		}
 	}
 }
 
