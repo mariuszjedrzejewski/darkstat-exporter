db2d0019d2f3fbc9f0830c8b5f7c0ad952e325e2
Mariusz Jędrzejewski <mariusz.jedrzejewski@gmail.com>
2023-03-26 16:25:01 +0200

    Initial version

---
+++ darkstat-exporter.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/go-co-op/gocron"
+)
+
+const (
+	URL_PREFIX = "http://openwrt.lan:667/hosts/%s"
+)
+
+type HostData struct {
+	Hostname   string
+	MacAddress string
+	In         float64
+	Out        float64
+	Total      float64
+}
+
+var (
+	inBytesCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "darkstat_in_bytes",
+			Help: "Incoming bytes",
+		},
+		[]string{"ip", "hostname", "mac_address"})
+
+	outBytesCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "darkstat_out_bytes",
+			Help: "Outgoing bytes",
+		},
+		[]string{"ip", "hostname", "mac_address"})
+
+	totalBytesCounter = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "darkstat_total_bytes",
+			Help: "Total bytes",
+		},
+		[]string{"ip", "hostname", "mac_address"})
+)
+
+func init() {
+	log.Println("Registering prometheus metrics...")
+	prometheus.MustRegister(inBytesCounter)
+	prometheus.MustRegister(outBytesCounter)
+	prometheus.MustRegister(totalBytesCounter)
+
+	log.Println("Getting initial metrics values...")
+	recordMetrics()
+
+	metricsRecordInterval := 30
+	if len(os.Getenv("METRICS_RECORD_INTERVAL")) != 0 {
+		interval, err := strconv.ParseInt(os.Getenv("METRICS_RECORD_INTERVAL"), 10, 64)
+		if err == nil {
+			metricsRecordInterval = int(interval)
+		}
+	}
+	log.Printf("Starting scheduler for every %d sec...", metricsRecordInterval)
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(metricsRecordInterval).Seconds().Do(recordMetrics)
+	s.StartAsync()
+}
+
+var recordMetrics = func() {
+	ipList := strings.Fields(os.Getenv("IP_LIST"))
+
+	for _, ip := range ipList {
+		url := fmt.Sprintf(URL_PREFIX, ip)
+
+		doc, err := goquery.NewDocument(url)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		dataToParse := make([]string, 0)
+
+		doc.Find("p").Each(func(x int, p *goquery.Selection) {
+			if x == 0 || x == 2 {
+				dataToParse = append(dataToParse, strings.Split(p.Text(), "\n")...)
+			}
+		})
+
+		d := getValues(dataToParse)
+
+		inBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.In))
+		outBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.Out))
+		totalBytesCounter.WithLabelValues(ip, d.Hostname, d.MacAddress).Set(float64(d.Total))
+	}
+}
+
+func getValues(data []string) HostData {
+	hd := HostData{}
+
+	for _, line := range data {
+		if len(line) == 0 {
+			continue
+		}
+
+		trimmedLine := strings.TrimLeft(line, " ")
+		lineData := strings.Split(trimmedLine, ": ")
+		rawValueStr := strings.ReplaceAll(lineData[1], ",", "")
+
+		switch lineData[0] {
+		case "Hostname":
+			hd.Hostname = rawValueStr
+			break
+		case "MAC Address":
+			hd.MacAddress = rawValueStr
+			break
+		case "In":
+			hd.In = getRawValue(rawValueStr)
+			break
+		case "Out":
+			hd.Out = getRawValue(rawValueStr)
+			break
+		case "Total":
+			hd.Total = getRawValue(rawValueStr)
+			break
+		}
+	}
+
+	return hd
+}
+
+func getRawValue(s string) float64 {
+	rawValue, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		log.Printf("Error converting value")
+		return 0
+	}
+	return rawValue
+}
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+
+	port := ":9090"
+	if len(os.Getenv("LISTEN_PORT")) != 0 {
+		port = os.Getenv("LISTEN_PORT")
+	}
+	log.Printf("Starting server at %s...", port)
+	http.ListenAndServe(port, nil)
+}
---
+++ go.mod
@@ -0,0 +1,25 @@
+module test01
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/go-co-op/gocron v1.19.0
+	github.com/prometheus/client_golang v1.14.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
